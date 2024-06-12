c90ab37231615dcf4a7f7439e9e90ce7bd4b1f6a
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-06-12 22:55:42 +0900

    signup페이지 스타일

---
+++ src/pages/Signup/style.ts
@@ -0,0 +1,150 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 12px;
+  align-items: center;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+`;
+
+export const SignupSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 47px 40px 39px 40px;
+  align-items: center;
+  border: 1px solid #dbdbdb;
+`;
+
+export const BigGrayText1 = styled.div`
+  padding-top: 10px;
+  font-size: 16px;
+  font-weight: 550;
+  color: #737373;
+`;
+
+export const BigGrayText = styled.div`
+  font-size: 16px;
+  font-weight: 550;
+  color: #737373;
+  padding-bottom: 18px;
+`;
+
+export const FacebookLoginBtn = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 9px 0;
+  background-color: #0095f6;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  border-radius: 8px;
+`;
+export const LoginBtnMessage = styled.div`
+  font-size: 14px;
+  font-weight: 550;
+  color: white;
+`;
+
+export const EmailInput = styled.input`
+  width: 100%;
+  height: 38px;
+  border: 1px solid #dbdbdb;
+  border-radius: 3px;
+  padding: 2px 8px;
+`;
+
+export const NameInput = styled.input`
+  width: 100%;
+  height: 38px;
+  border: 1px solid #dbdbdb;
+  border-radius: 3px;
+  padding: 2px 8px;
+  margin-top: 8px;
+`;
+
+export const UsernameInput = styled.input`
+  width: 100%;
+  height: 38px;
+  border: 1px solid #dbdbdb;
+  border-radius: 3px;
+  padding: 2px 8px;
+  margin-top: 8px;
+`;
+
+export const PwInput = styled.input`
+  width: 100%;
+  height: 38px;
+  border: 1px solid #dbdbdb;
+  border-radius: 3px;
+  padding: 2px 8px;
+  margin-top: 8px;
+`;
+
+export const LearnMore = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 14px;
+  align-items: center;
+  font-size: 11px;
+  color: #737373;
+`;
+
+export const LearnMoreMessages = styled.div`
+  display: flex;
+`;
+
+export const BlueText = styled.div`
+  color: #385898;
+`;
+
+export const Policy = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px 0 15px 0;
+  align-items: center;
+  font-size: 12px;
+  color: #737373;
+`;
+
+export const PolicyFlex = styled.div`
+  display: flex;
+  gap: 3px;
+`;
+
+export const SignupBtn = styled.button`
+  width: 100%;
+  padding: 7px 0;
+  background-color: #0095f6;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  font-size: 14px;
+  font-weight: 550;
+  cursor: pointer;
+`;
+
+export const LoginSection = styled.div`
+  display: flex;
+  gap: 2px;
+  margin-top: 10px;
+  width: 100%;
+  padding: 26px 0;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  border: 1px solid #dbdbdb;
+`;
+
+export const LoginBtn = styled.div`
+  color: #0095f6;
+  font-weight: 550;
+  cursor: pointer;
+`;
