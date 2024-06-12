fa0ec6fa087f3dcfd2ad767c09950f39725e34dc
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-06-12 22:57:38 +0900

    promotion컴포넌트 퍼블리싱

---
+++ src/Components/Promotion/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const PromotionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 17px;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const PromotionMessage = styled.div`
+  font-size: 14px;
+`;
+
+export const PromotionImgs = styled.div`
+  display: flex;
+  gap: 8px;
+`;
