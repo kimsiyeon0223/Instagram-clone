8dc874efa1ad2a936e6ba1b2dc6d36812138150d
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-06-12 23:01:03 +0900

    사용하지 않는 import 삭제

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,3 @@
-import Add from "../../assets/Add";
-import Find from "../../assets/Find";
-import Home from "../../assets/Home";
-import InstagramLogo from "../../assets/InstagramLogo";
-import Like from "../../assets/Like";
-import Message from "../../assets/Message";
-import Profile from "../../assets/Profile";
-import Reels from "../../assets/Reels";
-import Search from "../../assets/Search";
 import * as S from "./style";
 import Navbar from "../../Components/Navbar";
 import Centerbar from "../../Components/Centerbar";
