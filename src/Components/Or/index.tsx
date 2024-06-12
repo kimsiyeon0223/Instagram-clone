c0d51596a5c2a7e990c338c0795d23ad23f4070a
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-06-12 22:58:07 +0900

    or컴포넌트 퍼블리싱

---
+++ src/Components/Or/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./style";
+
+const Or = () => {
+  return (
+    <S.OrSection>
+      <S.HalfLine />
+      <S.Or>OR</S.Or>
+      <S.HalfLine />
+    </S.OrSection>
+  );
+};
+
+export default Or;
