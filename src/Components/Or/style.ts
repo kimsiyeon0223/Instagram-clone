bd6e343a44d4b614f89f808b7b093deb8942228e
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-06-12 22:58:17 +0900

    or컴포넌트 스타일

---
+++ src/Components/Or/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const OrSection = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 22px 0 30px 0;
+`;
+
+export const HalfLine = styled.div`
+  width: 40%;
+  height: 0;
+  border: 1px solid #dbdbdb;
+`;
+
+export const Or = styled.div`
+  padding: 0 20px;
+  color: #737373;
+  font-size: 13px;
+  font-weight: 550;
+`;
