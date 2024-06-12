7f9af3f7fdec6a9021b84f6841625531f251e51e
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-06-12 22:59:22 +0900

    addfriendbar컴포넌트 퍼블리싱

---
+++ src/Components/AddFriendbar/index.tsx
@@ -0,0 +1,62 @@
+import Profile from "../../assets/Profile";
+import * as S from "./style";
+
+const AddFriendbar = () => {
+  const profiles = [
+    {
+      id: 1,
+      name: "winternuary",
+      profile: <Profile width="50px" height="50px" />,
+    },
+    {
+      id: 2,
+      name: "jiwon07_1209",
+      profile: <Profile width="50px" height="50px" />,
+    },
+    {
+      id: 3,
+      name: "radiant._.17",
+      profile: <Profile width="50px" height="50px" />,
+    },
+    { id: 4, name: "m.jzi6", profile: <Profile width="50px" height="50px" /> },
+    {
+      id: 5,
+      name: "yxonxxze",
+      profile: <Profile width="50px" height="50px" />,
+    },
+  ];
+  return (
+    <S.Layout>
+      <S.MainSection>
+        <S.MyProfile>
+          <Profile width="50px" height="50px" />
+          <S.MyName>
+            <div style={{ fontWeight: "bold" }}>yxonxxze</div>
+            <div>김시연</div>
+          </S.MyName>
+          <S.ChangeBtn>전환</S.ChangeBtn>
+        </S.MyProfile>
+        <S.Recommend>
+          <S.ForMeRecommend>회원님을 위한 추천</S.ForMeRecommend>
+          <S.ShowAll>모두보기</S.ShowAll>
+        </S.Recommend>
+        <S.RecommendFriendSection>
+          {profiles.map((profile) => (
+            <S.RecommendFriend>
+              <div key={profile.id}>{profile.profile}</div>
+              <S.NameRecommned>
+                <>{profile.name}</>
+                <S.ForMeRecommend style={{ fontSize: "13px" }}>
+                  회원님을 위한 추천
+                </S.ForMeRecommend>
+              </S.NameRecommned>
+              <S.Follow>팔로우</S.Follow>
+            </S.RecommendFriend>
+          ))}
+        </S.RecommendFriendSection>
+      </S.MainSection>
+    </S.Layout>
+  );
+};
+
+export default AddFriendbar;
